50707bd5c637a01de9702344f2557d0b54872b4a
Ricardo Pulquerio <ricardo.pulquerio@unemat.br>
2023-01-12 15:47:51 -0400

    feat: adiciona disparo de email notifica inscricao realizada

--- app/api/v1/endpoints/students.py
+++ app/api/v1/endpoints/students.py
@@ -3,8 +3,10 @@ from pydantic.types import UUID4
 from typing import List
 from app import db
 from app.auth import auth
-
-from app import schema, models
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from app import schema, models, core
 
 router = APIRouter(prefix="/students", tags=["Students"])
 
@@ -83,5 +85,57 @@ def create_student_with_all(
     for data_prevjobs in prev_jobs:
         data_student.list_previouslyjobs.append(models.ListPreviouslyJobs(**data_prevjobs.dict()))
 
+    conteudo = """
+    <html>
+    <head>
+    <style type="text/css">
+        a {color: #d80a3e;}
+    body, #header h1, #header h2, p {margin: 0; padding: 0;}
+    #main {border: 1px solid #cfcece;}
+    img {display: block;}
+    #top-message p, #bottom p {color: #3f4042; font-size: 12px; font-family: Arial, Helvetica, sans-serif; }
+    #header h2 {color: #000000 !important; font-family: "Lucida Grande", sans-serif; font-size: 2px; }
+    #header h1 {color: #ffffff !important; font-family: "Lucida Grande", sans-serif; font-size: 24px; margin-bottom: 0!important; padding-bottom: 0; }
+    #header p {color: #ffffff !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; font-size: 12px;  }
+    h5 {margin: 0 0 0.8em 0;}
+        h5 {font-size: 18px; color: #444444 !important; font-family: Arial, Helvetica, sans-serif; }
+    p {font-size: 12px; color: #444444 !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; line-height: 1.5;}
+    </style>
+    </head>
+    <body>
+        <h2> UniEmpregos </h2>
+    <table width="100%" cellpadding="0" cellspacing="0" bgcolor="e4e4e4"><tr><td> 
+        <table id="main" width="600" align="center" cellpadding="0" cellspacing="15" bgcolor="ffffff">
+            <tr>
+                <td>
+                    <table id="header" cellpadding="10" cellspacing="0" align="center" bgcolor="8fb3e9">
+                    <tr>
+                        <td width="570" align="center"  bgcolor="#d80a3e"><h1>Inscrição Realizada Com Sucesso</h1></td>
+                    </tr>
+                    <tr>
+                        <td width="570" align="right" bgcolor="#d80a3e"><p>November 2017</p></td>
+                    </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </td></tr></table><!-- wrapper -->
+    
+    </body>
+    </html>
+    """
+    password = core.config.settings.SMTP_PASSWORD
+    msg = MIMEMultipart()
+    msg['Subject'] = "UniEmpregos"
+    msg['From'] = core.config.settings.SMTP_USER
+    msg['To'] = student.email
+    msg.attach(MIMEText(conteudo, 'html'))
+
+    with smtplib.SMTP_SSL(host='smtp.gmail.com', timeout=5, port=465) as smtp:
+        smtp.starttls
+        smtp.login(msg['From'], password)
+        smtp.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+        smtp.quit()
+        print('enviado')
 
     return data_student.create()
