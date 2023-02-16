2138ed3fa0d8e62b7a3fb1b8d48b3dbd8d5fd5fe
Ricardo Pulquerio <ricardo.pulquerio@unemat.br>
2023-02-16 15:24:23 -0400

    refactor: altera schema PutCampus, addresss_uuid e course_uuid troca de str para UUID

--- app/schema/campus_schema.py
+++ app/schema/campus_schema.py
@@ -37,8 +37,8 @@ class PutCampus(BaseModel):
     # creat_at: datetime | None = Field(description='Creat_at Documentar')
     # updat_at: datetime | None = Field(description='Updat_at Documentar')
     name: str | None = Field(description='Name Documentar', max_length=255)
-    address_uuid: str | None = Field(description='Address_uuid Documentar')
-    course_uuid: str | None = Field(description='Course_uuid Documentar')
+    address_uuid: UUID | None = Field(description='Address_uuid Documentar')
+    course_uuid: UUID | None = Field(description='Course_uuid Documentar')
     name_institution: str | None = Field(description='Name_institution Documentar', max_length=255)
     type_institution: str | None = Field(description='Type_institution Documentar', max_length=255)
 
