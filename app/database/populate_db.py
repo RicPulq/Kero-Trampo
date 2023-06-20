7503d3824004c4eb1f4dac8ead72eaa5c54f7f34
Ricardo Pulquerio <ricardo.pulquerio@unemat.br>
2023-06-20 17:02:54 -0400

    feat: adiciona pop_db_init

--- app/database/populate_db.py
+++ app/database/populate_db.py
@@ -3,11 +3,22 @@ from app import models, schema
 from .user import admin
 from .role import role
 
-def pop_db():
-    for data in role:
-        role_schema = schema.Post
-    user_schema = schema.PostUser(**admin)
-    user = models.User(**user_schema.dict())
-    user.create()
+def pop_db_init():
+    for role_aux in role:
+        print(role_aux["name"])
+        if not models.Role.query_com_dois_params("name",role_aux["name"],None,None):
+            role_schema = schema.PostRole
+            role_schema = dict(role_aux)
+            print(role_schema)
+            roles = models.Role(**role_schema)
+            roles.flush()
+    print(admin)
+    if not models.User.query_com_dois_params("username",admin["username"],None,None):
+        user_schema = schema.PostUser(**admin)
+        print(admin)
+        user = models.User(**user_schema.dict())
+        user.flush()
     
+def pop_db():
+    pass
 
--- app/database/role.py
+++ app/database/role.py
@@ -1,4 +1,6 @@
-role = [{
-    "name": "Admin",
-    "permission_level": 5
-}]
+role = [
+    {"name": "admin", "permission_level": 5},
+    {"name": "student", "permission_level": 1},
+    {"name": "course", "permission_level": 2},
+    {"name": "company", "permission_level": 3},
+]
