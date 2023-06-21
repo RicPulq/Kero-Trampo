cf8bf929efa614086a088dcc8687ed4f64326f16
Ricardo Pulquerio <ricardo.pulquerio@unemat.br>
2023-06-21 16:07:05 -0400

    feat: cria pop_db_init (cria roles e o usuario admin) e executa

--- app/database/populate_db.py
+++ app/database/populate_db.py
@@ -1,23 +1,29 @@
+from uuid import UUID
 from app import models, schema
 
 from .user import admin
 from .role import role
+from ..core.security import get_password_hash
 
 def pop_db_init():
     for role_aux in role:
-        print(role_aux["name"])
+    # percorre toda a lista de dados "role"
         if not models.Role.query_com_dois_params("name",role_aux["name"],None,None):
+        # verifica no banco se já existe os dados, caso não cria no banco as "roles" 
             role_schema = schema.PostRole
             role_schema = dict(role_aux)
-            print(role_schema)
             roles = models.Role(**role_schema)
-            roles.flush()
-    print(admin)
+            roles.create()
     if not models.User.query_com_dois_params("username",admin["username"],None,None):
+    # verifica no banco se já existe o usuário "admin" e caso não cria
+        admin["password"] = get_password_hash(admin["password"])
+        admin["active"] = True
+        aux_role = role[0]
+        data_role = models.Role.query_com_dois_params("name",aux_role["name"],"permission_level",aux_role["permission_level"])
+        admin["role_uuid"] = data_role.uuid
         user_schema = schema.PostUser(**admin)
-        print(admin)
         user = models.User(**user_schema.dict())
-        user.flush()
+        user.create()
     
 def pop_db():
     pass
