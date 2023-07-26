4beea1b904502d10f23c821d432699f61dfe1ffc
Ricardo Pulquerio <ricardo.pulquerio@unemat.br>
2023-07-26 13:30:47 -0400

    refactor: alterar html de mensagem no email

--- app/templates/successfully.py
+++ app/templates/successfully.py
@@ -1,36 +1,39 @@
 conteudo = """
-    <html>
+   <html>
     <head>
-    <style type="text/css">
-        a {color: #d80a3e;}
-    body, #header h1, #header h2, p {margin: 0; padding: 0;}
-    #main {border: 1px solid #cfcece;}
-    img {display: block;}
-    #top-message p, #bottom p {color: #3f4042; font-size: 12px; font-family: Arial, Helvetica, sans-serif; }
-    #header h2 {color: #000000 !important; font-family: "Lucida Grande", sans-serif; font-size: 2px; }
-    #header h1 {color: #ffffff !important; font-family: "Lucida Grande", sans-serif; font-size: 24px; margin-bottom: 0!important; padding-bottom: 0; }
-    #header p {color: #ffffff !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; font-size: 12px;  }
-    h5 {margin: 0 0 0.8em 0;}
-        h5 {font-size: 18px; color: #444444 !important; font-family: Arial, Helvetica, sans-serif; }
-    p {font-size: 12px; color: #444444 !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; line-height: 1.5;}
-    </style>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     </head>
-    <body>
-        <h2> KeroTrampo </h2>
-    <table width="100%" cellpadding="0" cellspacing="0" bgcolor="e4e4e4"><tr><td> 
-        <table id="main" width="600" align="center" cellpadding="0" cellspacing="15" bgcolor="ffffff">
-            <tr>
-                <td>
-                    <table id="header" cellpadding="10" cellspacing="0" align="center" bgcolor="8fb3e9">
-                    <tr>
-                        <td width="570" align="center"  bgcolor="#0000CD"><h1>Inscrição Realizada Com Sucesso</h1></td>
-                    </tr>
-                    </table>
-                </td>
-            </tr>
-        </table>
-    </td></tr></table><!-- wrapper -->
-    
+    <body style="width: 100%; display: flex; flex-direction: column; align-items: center;">
+        <div style="background-color: #062e69; max-width: 450px; width: 100%; margin: 0; padding: 20px; border-radius: 40px;">
+            <table>
+                <tr>
+                    <td>
+                        <img style="max-width: 270px; width: 50%; height: auto; margin: 20px 25%;" src="https://risc.unemat.br/egresso/wp-content/uploads/2023/04/Frame-2117-2.png" alt="logo KeroTrampo">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h2 style="color: #fff;font-family: 'Poppins'; font-size: 22px; margin: 0; text-align: center; text-shadow: 5px 5px 8px #000000aa;">Sejam bem-vindos no site</h2>
+                        <h2 style="color: #fff;font-family: 'Poppins'; font-size: 26px; margin: 0; text-align: center; text-shadow: 5px 5px 8px #000000aa;">KERO TRAMPO.</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div style="height: 10px;"></div>
+                        <p style="color: #fff;font-family: 'Poppins'; text-align: center; font-size: 18px; margin: 0;">Obrigado por se cadastrar, aproveitem as vantagens e oportunidades que o site oferece!</p>
+                        <div style="border: solid 1px #fff; margin: 20px 30px 20px 30px;"></div>
+                        <p style="color: #fff;font-family: 'Poppins'; text-align: center; font-size: 18px; margin: 0;">Este é um e-mail automático.</p>
+                        <p style="color: #fff;font-family: 'Poppins'; text-align: center; font-size: 18px; margin: 0;">Para falar conosco, entre em contato:</p>
+                        <div style="height: 10px;"></div>
+                        <a href="mailto:email@example.com" style="font-family: 'Poppins'; cursor: pointer; display: block; color: #d7ae45; text-decoration: none; text-align: center;">
+                            <span style=" font-family: 'Poppins'; text-align: center; font-size: 18px; margin: 10px 0;">email@example.com</span>
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </body>
-    </html>
+</html>
     """
